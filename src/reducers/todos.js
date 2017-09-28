6ea1413b3355b2c44c79ceb8c990a99fce21d96e
Daniel Arnaudov <darna@softserveinc.com>
2017-09-28 18:40:20 +0300

    Normilized todos data

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,38 +1,68 @@
+import { combineReducers } from 'redux';
+import _ from 'lodash';
 import todo from './todo';
 import actionTypes from '../actions/actionTypes';
 import { VisibilityFilter } from '../constants';
 
-function todos(state = [], action) {
+function byId(state = {}, action) {
     switch (action.type) {
         case actionTypes.ADD_TODO:
-            return [
-                ...state,
-                todo(null, action)
-            ];
         case actionTypes.TOGGLE_TODO:
-            return state.map(curTodo => todo(curTodo, action));
+            return {
+                ...state,
+                [action.id]: todo(state[action.id], action)
+            };
         case actionTypes.DELETE_TODO:
-            return state.filter(todo => todo.id !== action.id);
+            return _.omit(state, action.id);
         default:
             return state;
     }
 }
 
+function allIds(state = [], action) {
+    switch (action.type) {
+        case actionTypes.ADD_TODO:
+            return [
+                ...state,
+                action.id
+            ];
+        case actionTypes.DELETE_TODO:
+            return state.filter((id) => {
+                return id !== action.id;
+            })
+        default:
+            return state;
+    }
+}
+
+const todos = combineReducers({
+    byId,
+    allIds
+});
+
 export default todos;
 
+function getAllTodos(state) {
+    return state.allIds.map((id) => {
+        return state.byId[id];
+    });
+}
+
 export const getVisibleTodos = (state, filter) => {
+    const allTodos = getAllTodos(state);
+    
     switch (filter) {
         case VisibilityFilter.All:
-            return state;
+            return allTodos;
         case VisibilityFilter.ACTIVE:
-            return state.filter((todo) => {
+            return allTodos.filter((todo) => {
                 return !todo.completed;
             });
         case VisibilityFilter.COMPLETED:
-            return state.filter((todo) => {
+            return allTodos.filter((todo) => {
                 return todo.completed;
             });
         default:
-            return state;
+            return allTodos;
     }
 }
